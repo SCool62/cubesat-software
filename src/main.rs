22680fa95b23430fe854274781aa50af5809db28
SCool62 <samthecool300@icloud.com>
2025-02-20 18:49:31 -0800

    Initial overcurrent handling code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 #![no_std]
 #![no_main]
+#![feature(impl_trait_in_assoc_type)]
 
 use embassy_executor::Spawner;
+use embassy_stm32::{exti::Channel, gpio::{Pin, Pull}, Config};
 use embassy_time::Timer;
 
 use panic_reset as _;
+use tasks::respond_oc;
+
+mod tasks;
+
+
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
+    let p = embassy_stm32::init(Config::default());
+
+    macro_rules! bind_oc_task {
+        ($pin:expr, $channel:expr) => {
+            let pin = $pin.degrade();
+            let ch = $channel.degrade();
+            
+
+            // TODO: MAKE SURE PULL IS CORRECT
+            spawner.must_spawn(respond_oc::watch_oc(pin, ch, Pull::Down));
+        };
+    }
+
+    bind_oc_task!(p.PC13, p.EXTI13);
+
     loop {
         Timer::after_millis(2).await;
     }
