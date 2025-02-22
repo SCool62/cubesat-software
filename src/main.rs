6eceae365a3c8d2f081f9fe6956974755cb11c5c
SCool62 <samthecool300@icloud.com>
2025-02-22 12:04:31 -0800

    Add signals for each CM circuit

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,29 @@
 #![feature(impl_trait_in_assoc_type)]
 
 use embassy_executor::Spawner;
-use embassy_stm32::{exti::Channel, gpio::{Pin, Pull}, Config};
+use embassy_stm32::{Config, exti::Channel, gpio::Pin};
 use embassy_time::Timer;
 
 use panic_reset as _;
-use tasks::respond_oc;
+use tasks::current_monitoring;
 
 mod tasks;
-
-
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Config::default());
 
     macro_rules! bind_oc_task {
-        ($pin:expr, $channel:expr) => {
+        ($pin:expr, $channel:expr, $channel_num:literal) => {
             let pin = $pin.degrade();
             let ch = $channel.degrade();
-            
 
             // TODO: MAKE SURE PULL IS CORRECT
-            spawner.must_spawn(respond_oc::watch_oc(pin, ch, Pull::Down));
+            spawner.must_spawn(current_monitoring::watch_oc(pin, ch, $channel_num));
         };
     }
 
-    bind_oc_task!(p.PC13, p.EXTI13);
+    bind_oc_task!(p.PC0, p.EXTI0, 0);
 
     loop {
         Timer::after_millis(2).await;
